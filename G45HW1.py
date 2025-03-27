309665c9992c4e13d7063c9d250511c7453321b0
GiovaGa <48856010+GiovaGa@users.noreply.github.com>
2025-03-27 21:54:32 +0100

    Template for Homework 1

---
+++ G45HW1.py
@@ -0,0 +1,16 @@
+from pyspark import SparkContext, SparkConf
+
+# giova
+def MRComputeStandardObjective(U,C):
+    pass
+
+def MRComputeFairObjective(U,C):
+    pass
+
+# simpatine
+def MRPrintStatistics(U,C):
+    pass
+
+# Giordano
+if __name__ == "__main__":
+    pass
