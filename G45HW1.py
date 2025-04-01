b92b908cecfa3ac520397a13c54e9636e5701e90
GiovaGa <48856010+GiovaGa@users.noreply.github.com>
2025-04-01 13:00:53 +0200

    Simple implementation of MRComputeStandardObjective

--- G45HW1.py
+++ G45HW1.py
@@ -5,9 +5,42 @@ import numpy as np
 
 # giova
 def MRComputeStandardObjective(U, C):
-    pass
+    """
+This function computes the standard K-means clustering cost function on a set of points U with centers C.
+
+Parameters
+----------
+U : pyspark.RDD
+    The set of data points as (pos, category) where pos is a vector and category is either "A" or "B".
+C : iterable
+    The centers
+
+Returns
+-------
+float
+    The value of the cost function
+    """
+
+    d = (U.flatMap(lambda x : [(x, np.square(np.linalg.norm(x[0]-c))) for c in C])
+         .reduceByKey(min)).flatMap(lambda x: [x[1]])
+    return d.mean()
 
 def MRComputeFairObjective(U, C):
+    """
+This function computes the fair K-means clustering cost function on a set of points U with centers C.
+
+Parameters
+----------
+U : pyspark.RDD
+    The set of data points as (pos, category) where pos is a vector and category is either "A" or "B".
+C : iterable
+    The centers
+
+Returns
+-------
+float
+    The value of the cost function
+    """
     pass
 
 # simpatine
@@ -16,7 +49,7 @@ def MRPrintStatistics(U, C):
 
 def parse_line(line):
     parts = line.strip().split(',')
-    point = np.array([float(x) for x in parts[:-1]])  
+    point = np.array([float(x) for x in parts[:-1]])
     group = parts[-1]
     return (point, group)
 
@@ -24,35 +57,35 @@ def main():
     if len(sys.argv) != 5:
         print("Usage: G45HW1.py <file_path> <L> <K> <M>")
         sys.exit(1)
-    
+
     file_path, L, K, M = sys.argv[1], int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4])
-    
+
     sc = SparkContext(appName="FairKMeans")
-    
+
     points_rdd = sc.textFile(file_path, minPartitions=L).map(parse_line).cache()
-    
+
     N = points_rdd.count()
     NA = points_rdd.filter(lambda x: x[1] == 'A').count()
     NB = points_rdd.filter(lambda x: x[1] == 'B').count()
-    
+
     print(f"======= OUTPUT FOR L = {L}, K = {K}, M = {M} =======")
     print(f"\nInput file = {file_path}, L = {L}, K = {K}, M = {M}")
     print(f"N = {N}, NA = {NA}, NB = {NB}")
-    
+
 
     vectors_rdd = points_rdd.map(lambda x: np.array(x[0]))
     model = KMeans.train(vectors_rdd, K, maxIterations=M)
     centroids = model.clusterCenters
-    
+
     delta = MRComputeStandardObjective(points_rdd, centroids)
     phi = MRComputeFairObjective(points_rdd, centroids)
-    
+
     print(f"Delta(U,C) = {delta:.6f}")
     print(f"Phi(A,B,C) = {phi:.6f}")
-    
+
     MRPrintStatistics(points_rdd, centroids)
-    
+
     sc.stop()
-    
+
 if __name__ == "__main__":
     main()
