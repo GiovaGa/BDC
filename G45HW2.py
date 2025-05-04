3a9681370f685f301ed93dd00205e42b19182112
GiovaGa <48856010+GiovaGa@users.noreply.github.com>
2025-05-04 20:24:59 +0200

    MRFairLloyd should work now

--- G45HW2.py
+++ G45HW2.py
@@ -61,9 +61,38 @@ Returns
 -------
 list of the centers
     """
+    C = [np.array(p) for (p,c) in U.take(K)]
+
+    UA = U.filter(lambda x : x[1] == 'A')
+    UB = U.filter(lambda x : x[1] == 'B')
+    countA = UA.count()
+    countB = UB.count()
+
     for i in range(M):
-        pass
-    pass
+        T = 10; gamma = 0.5
+        x = np.zeros(K)
+        print("C:", C)
+        partitionsA = UA.map(lambda x : (np.argmin([np.square(np.array(x[0])-c).sum() for c in C]),x[0])).groupByKey()
+        a = np.array(partitionsA.map(lambda p : len(p)/countA).collect())
+        muA = np.array(partitionsA.mapValues(lambda p : np.array(list(p)).mean(axis=0)).sortByKey().map(lambda x : x[1]).collect())
+        # print("muA:", muA)
+
+        partitionsB = UB.map(lambda x : (np.argmin([np.square(np.array(x[0])-c).sum() for c in C]),x[0])).groupByKey()
+        b = np.array(partitionsB.map(lambda p : len(p)/countB).collect())
+        muB = np.array(partitionsB.mapValues(lambda p : np.array(list(p)).mean(axis=0)).sortByKey().map(lambda x : x[1]).collect())
+        # print("muB:", muB)
+        l = np.linalg.norm(muA-muB,axis=1)
+        # print("l:", l)
+        for t in range(T):
+            x = ((1-gamma)*b*l)/(gamma*a+(1-gamma)*b)
+            FA = MRComputeStandardObjective(UA, x)
+            FB = MRComputeStandardObjective(UB, x)
+            gamma += (1 if FA > FB else -1)*(0.5)**(-t-2)
+        C = ((l-x)[:,np.newaxis]*muA + x[:,np.newaxis]*muB)/l[:,np.newaxis]
+    return C
+
+
+
 
 def parse_line(line):
     parts = line.strip().split(',')
@@ -90,10 +119,11 @@ def main():
     print(f"\nInput file = {file_path}, L = {L}, K = {K}, M = {M}")
     print(f"N = {N}, NA = {NA}, NB = {NB}")
 
-
     vectors_rdd = points_rdd.map(lambda x: tuple(x[0]))
     model = KMeans.train(vectors_rdd, K, maxIterations=M)
     centroids = model.clusterCenters
+
+    fair_centroids = MRFairLloyd(points_rdd, K, M)
 
     delta = MRComputeStandardObjective(points_rdd, centroids)
     phi = MRComputeFairObjective(points_rdd, centroids)
@@ -101,7 +131,7 @@ def main():
     print(f"Delta(U,C) = {delta:.6f}")
     print(f"Phi(A,B,C) = {phi:.6f}")
 
-    MRPrintStatistics(points_rdd, centroids)
+    # MRPrintStatistics(points_rdd, centroids)
 
     sc.stop()
 
